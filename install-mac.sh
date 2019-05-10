4c570a33279d398dd9d2a5607407f4d8974c97f6
Dean Hart <deanhart@amazon.com>
2019-05-10 11:29:57 -0600

    fixed issue with install script not finding openssl

--- install-mac.sh
+++ install-mac.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 brew install openssl libuv cmake
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
@@ -6,8 +8,10 @@ patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
-OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
-cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+
+OPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2r
+
+cmake -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR -DOPENSSL_LIBRARIES=$OPENSSL_ROOT_DIR/lib ..
 make 
 sudo make install
 cd ..
